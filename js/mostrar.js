3cd6433e066ff435d7f16cfac4dca97ba1dad43d
crismi16 <migueltulmo@yahoo.es>
2024-01-19 17:48:29 -0500

    añadido

---
+++ js/mostrar.js
@@ -0,0 +1,77 @@
+function mostrar1(){
+    document.getElementById('Pasteles').style.display='block';
+    ocument.getElementById('Galletas').style.display='none';
+}
+function mostrar2(){
+    document.getElementById('Galletas').style.display='block';
+    document.getElementById('Pasteles').style.display='none';
+}
+// let contenedorMensaje1=document.querySelector("#btn-pastel");
+// let contenedorMensaje2=document.querySelector("#Pasteles");
+// const mostrar=(elemento,evento)=>{
+//     if(evento='click'){
+//         elemento.style.display='block';
+//     }
+//     // switch(evento){
+//     //     case 'click':
+//     //         elemento.style.display='block';
+//     //         break;
+//     //         case 'mouseout':
+//     //         elemento.style.display='none';
+//     // }
+// }
+// contenedorMensaje1.addEventListener('click',()=>{
+//     mostrar(contenedorMensaje2,'click')
+// })
+// contenedorMensaje1.addEventListener('mouseout',()=>{
+//     mostrar(contenedorMensaje2,'mouseout')
+// }
+// let contenedorMensaje1=document.querySelector("#contenedor1");
+// let contenedorMensaje2=document.querySelector("#contenedor2");
+// let contenedorMensaje3=document.querySelector("#contenedor3");
+// let contenedorMensaje4=document.querySelector("#contenedor4");
+// let contenedorMensaje5=document.querySelector("#contenedor5");
+// let contenedorMensaje6=document.querySelector("#contenedor6");
+// let contenedorMensaje7=document.querySelector("#contenedor7");
+// let contenedorMensaje8=document.querySelector("#contenedor8");
+// let contenedorMensaje9=document.querySelector("#contenedor9");
+// let contenedorMensaje10=document.querySelector("#contenedor10");
+// const mostrar=(elemento,evento)=>{
+//     switch(evento){
+//         case 'mouseover':
+//             elemento.style.display='block';
+//             break;
+//             case 'mouseout':
+//             elemento.style.display='none';
+//     }
+// }
+// contenedorMensaje1.addEventListener('mouseover',()=>{
+//     mostrar(contenedorMensaje2,'mouseover')
+// })
+// contenedorMensaje1.addEventListener('mouseout',()=>{
+//     mostrar(contenedorMensaje2,'mouseout')
+// })
+// contenedorMensaje3.addEventListener('mouseover',()=>{
+//     mostrar(contenedorMensaje4,'mouseover')
+// })
+// contenedorMensaje3.addEventListener('mouseout',()=>{
+//     mostrar(contenedorMensaje4,'mouseout')
+// })
+// contenedorMensaje5.addEventListener('mouseover',()=>{
+//     mostrar(contenedorMensaje6,'mouseover')
+// })
+// contenedorMensaje5.addEventListener('mouseout',()=>{
+//     mostrar(contenedorMensaje6,'mouseout')
+// })
+// contenedorMensaje7.addEventListener('mouseover',()=>{
+//     mostrar(contenedorMensaje8,'mouseover')
+// })
+// contenedorMensaje7.addEventListener('mouseout',()=>{
+//     mostrar(contenedorMensaje10,'mouseout')
+// })
+// contenedorMensaje9.addEventListener('mouseover',()=>{
+//     mostrar(contenedorMensaje10,'mouseover')
+// })
+// contenedorMensaje9.addEventListener('mouseout',()=>{
+//     mostrar(contenedorMensaje10,'mouseout')
+// })
